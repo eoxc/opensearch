ca16bb6ae1628194ce497ee2014b55e04a398989
Fabian Schindler <fabian.schindler.strauss@gmail.com>
2016-08-09 18:22:38 +0200

    Cleanup

--- src/index.js
+++ src/index.js
@@ -1,15 +1,8 @@
-// TODO: allow client to choose whether or not to use the polyfills
-
-import { polyfill } from 'es6-promise';
-polyfill();
-
 import { OpenSearchService } from './service';
 import { fetchAndCheck } from './utils';
 
-export async function discover(url) {
-  const response = await fetchAndCheck(url);
-  if (response.status >= 400) {
-    throw new Error('Bad response from server');
-  }
-  return new OpenSearchService(response.text());
+export function discover(url) {
+  return fetchAndCheck(url)
+    .then(response => response.text())
+    .then(response => new OpenSearchService(xml));
 }
