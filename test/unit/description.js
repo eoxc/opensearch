fae66ed6a69ad2004f300d520f6bc59a20cc3f72
Fabian Schindler <fabian.schindler.strauss@gmail.com>
2016-08-12 12:01:29 +0200

    Adding XML files for testing.

--- test/unit/description.js
+++ test/unit/description.js
@@ -2,32 +2,7 @@ import { expect } from 'chai';
 
 import { OpenSearchDescription } from '../../src/description.js';
 
-const xml = `<?xml version="1.0" encoding="UTF-8"?>
- <OpenSearchDescription xmlns="http://a9.com/-/spec/opensearch/1.1/">
-   <ShortName>Web Search</ShortName>
-   <Description>Use Example.com to search the Web.</Description>
-   <Tags>example web</Tags>
-   <Contact>admin@example.com</Contact>
-   <Url type="application/atom+xml"
-        template="http://example.com/?q={searchTerms}&amp;pw={startPage?}&amp;format=atom"/>
-   <Url type="application/rss+xml"
-        template="http://example.com/?q={searchTerms}&amp;pw={startPage?}&amp;format=rss"/>
-   <Url type="text/html"
-        template="http://example.com/?q={searchTerms}&amp;pw={startPage?}"/>
-   <LongName>Example.com Web Search</LongName>
-   <Image height="64" width="64" type="image/png">http://example.com/websearch.png</Image>
-   <Image height="16" width="16" type="image/vnd.microsoft.icon">http://example.com/websearch.ico</Image>
-   <Query role="example" searchTerms="cat" />
-   <Developer>Example.com Development Team</Developer>
-   <Attribution>
-     Search data Copyright 2005, Example.com, Inc., All Rights Reserved
-   </Attribution>
-   <SyndicationRight>open</SyndicationRight>
-   <AdultContent>false</AdultContent>
-   <Language>en-us</Language>
-   <OutputEncoding>UTF-8</OutputEncoding>
-   <InputEncoding>UTF-8</InputEncoding>
- </OpenSearchDescription>`;
+const xml = require('./data/OSDD_example.xml');
 
 
 describe('OpenSearchDescription', () => {
@@ -64,7 +39,7 @@ describe('OpenSearchDescription', () => {
   describe('#getUrl', () => {
     const description = new OpenSearchDescription(xml);
     it('should return the correct URL object', () => {
-      const url = description.getUrl('text/html');
+      const url = description.getUrl({ searchTerms: 'test' }, 'text/html');
       expect(url.type).to.equal('text/html');
       expect(url.method).to.equal('GET');
     });
--- test/unit/service.js
+++ test/unit/service.js
@@ -1,21 +1,37 @@
+import { expect } from 'chai';
 import { OpenSearchService } from '../../src/service.js';
+import fetchMock from 'fetch-mock';
+
+const osddExample = require('./data/OSDD_example.xml');
+const atomExample = require('./data/atom_example.xml');
+
+require('es6-promise').polyfill();
 
 describe('OpenSearchService', () => {
-  describe('constructor', () => {
-
+  before(() => {
+    fetchMock
+      .mock('^http://example.com/?q=', atomExample)
+      .mock('http://example.com/', osddExample);
   });
-    // describe('Greet function', () => {
-    //     beforeEach(() => {
-    //         spy(babelWebpackBoilerplate, 'greet');
-    //         babelWebpackBoilerplate.greet();
-    //     });
-    //
-    //     it('should have been run once', () => {
-    //         expect(babelWebpackBoilerplate.greet).to.have.been.calledOnce;
-    //     });
-    //
-    //     it('should have always returned hello', () => {
-    //         expect(babelWebpackBoilerplate.greet).to.have.always.returned('hello');
-    //     });
-    // });
+  after(() => {
+    fetchMock.restore();
+  });
+
+  describe('constructor', () => {
+    it('shall parse the description', () => {
+      const service = new OpenSearchService(osddExample);
+      expect(service).to.exist;
+      expect(service.descriptionDocument).to.exist;
+    });
+  });
+
+  describe('#search()', () => {
+    const service = new OpenSearchService(osddExample);
+    it('shall search', () => {
+      return service.search({ searchTerms: 'terms' })
+        .then((results) => {
+          expect(results).to.have.lengthOf(1);
+        });
+    });
+  });
 });
