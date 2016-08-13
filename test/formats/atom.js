440a359ca4670b6ac1c78adff8c704cd88b985e2
Fabian Schindler <fabian.schindler.strauss@gmail.com>
2016-08-13 22:06:28 +0200

    Adding setup script. Adapting tests for formats API.

--- test/formats/atom.js
+++ test/formats/atom.js
@@ -10,30 +10,35 @@ describe('AtomFormat', () => {
   const format = new AtomFormat();
   describe('parse', () => {
     it('should parse the entries of a simple example', () => {
-      expect(format.parse(atomExample)).to.deep.equal([{
-        id: 'urn:uuid:1225c695-cfb8-4ebb-aaaa-80da344efa6a',
-        properties: {
-          title: 'New York History',
-          updated: new Date('2003-12-13T18:30:02Z'),
-          content: `
+      return format.parse(new Response(atomExample)).then(result => {
+        expect(result).to.deep.equal([{
+          id: 'urn:uuid:1225c695-cfb8-4ebb-aaaa-80da344efa6a',
+          properties: {
+            title: 'New York History',
+            updated: new Date('2003-12-13T18:30:02Z'),
+            content: `
       ... Harlem.NYC - A virtual tour and information on
       businesses ...  with historic photos of Columbia's own New York
       neighborhood ... Internet Resources for the City's History. ...
     `,
-        },
-      }]);
+          },
+        }]);
+      });
+
     });
 
     it('should parse the entries of a geo example', () => {
-      expect(format.parse(xmlGeoBox)).to.deep.equal([{
-        id: 'dlr_fire_emission_dispersion_california_20160223_1',
-        properties: {
-          title: 'dlr_fire_emission_dispersion_california_20160223_1',
-          updated: new Date('2003-12-13T18:30:02Z'),
-          content: '',
-        },
-        bbox: [-135.0, 0.0, -90.0, 45.0],
-      }]);
+      return format.parse(new Response(xmlGeoBox)).then(result => {
+        expect(result).to.deep.equal([{
+          id: 'dlr_fire_emission_dispersion_california_20160223_1',
+          properties: {
+            title: 'dlr_fire_emission_dispersion_california_20160223_1',
+            updated: new Date('2003-12-13T18:30:02Z'),
+            content: '',
+          },
+          bbox: [-135.0, 0.0, -90.0, 45.0],
+        }]);
+      });
     });
   });
 });
--- test/formats/rss.js
+++ test/formats/rss.js
@@ -10,29 +10,33 @@ describe('RSSFormat', () => {
   const format = new RSSFormat();
   describe('parse', () => {
     it('should parse the entries of a simple example', () => {
-      expect(format.parse(rssSimple)).to.deep.equal([{
-        id: '',
-        properties: {
-          title: 'New York History',
-          content: `
+      return format.parse(new Response(rssSimple)).then(result => {
+        expect(result).to.deep.equal([{
+          id: '',
+          properties: {
+            title: 'New York History',
+            content: `
         ... Harlem.NYC - A virtual tour and information on
         businesses ...  with historic photos of Columbia's own New York
         neighborhood ... Internet Resources for the City's History. ...
       `,
-        },
-      }]);
+          },
+        }]);
+      });
     });
 
     it('should parse the entries of a geo example', () => {
-      expect(format.parse(rssBox)).to.deep.equal([{
-        id: 'http://ows.eox.at/testbed-12_staging/eoxserver/opensearch/collections/dlr_fire_emission_dispersion_california_20160223/rss/?q=&count=1&startIndex=&bbox=&geom=&lon=&lat=&r=&georel=&uid=&start=&timerel=',
-        properties: {
-          title: 'dlr_fire_emission_dispersion_california_20160223_1',
-          content: '',
-          time: new Date('2016-02-23T03:00:00.000Z'),
-        },
-        bbox: [-135.0, 0.0, -90.0, 45.0],
-      }]);
+      return format.parse(new Response(rssBox)).then(result => {
+        expect(result).to.deep.equal([{
+          id: 'http://ows.eox.at/testbed-12_staging/eoxserver/opensearch/collections/dlr_fire_emission_dispersion_california_20160223/rss/?q=&count=1&startIndex=&bbox=&geom=&lon=&lat=&r=&georel=&uid=&start=&timerel=',
+          properties: {
+            title: 'dlr_fire_emission_dispersion_california_20160223_1',
+            content: '',
+            time: new Date('2016-02-23T03:00:00.000Z'),
+          },
+          bbox: [-135.0, 0.0, -90.0, 45.0],
+        }]);
+      });
     });
   });
 });
